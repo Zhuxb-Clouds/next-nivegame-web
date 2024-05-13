d318319e6aa6d0276f1aa5ae23512459ca4a6047
Zhuxb <ZhuxbClouds@gmail.com>
2024-05-14 04:07:29 +0800

    feat: Update metadata in layout.tsx
    The metadata in layout.tsx has been updated to include additional information such as icons and authors.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,16 @@ import Footer from "@/components/Footer";
 import React, { useState, useEffect } from "react";
 import "./globals.css";
 import "./post.css";
-
+import type { Metadata } from "next";
 type LayoutProps = {
   children: React.ReactNode;
 };
 
-export const metadata = {
-  title: "飞雪 - 武侠 | 桌游 | 话说"
-}
+export const metadata: Metadata = {
+  title: "飞雪 - 武侠 | 桌游 | 话说",
+  icons: "/icon.png",
+  authors: { name: "朱仙变", url: "https://zhuxb.dev/" },
+};
 
 export default function Layout({ children }: LayoutProps) {
   return (

fcc8fbedffb4ad954c6b2d5c8ebc8d4347a11ddd
Zhuxb <ZhuxbClouds@gmail.com>
2024-09-09 05:02:21 +0800

    Refactor footer link to open in new tab

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p>© 2024 飞雪 - 武侠 | 桌游 | 话说</p>
-      <a href="beian.miit.gov.cn">鄂ICP备2022011304号-1</a>
+      <a href="https://beian.miit.gov.cn" target="_blank">鄂ICP备2022011304号-1</a>
     </footer>
   );
 }

caceb5f527fa84b774ad7016bc84bd0940f17391
Zhuxb <ZhuxbClouds@gmail.com>
2024-09-09 04:59:47 +0800

    Refactor footer styles for mobile responsiveness

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,8 @@
-export default function Footer()  {
+export default function Footer() {
   return (
     <footer>
       <p>© 2024 飞雪 - 武侠 | 桌游 | 话说</p>
+      <a href="beian.miit.gov.cn">鄂ICP备2022011304号-1</a>
     </footer>
-  )
+  );
 }
